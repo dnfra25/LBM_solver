adafbc64155a04e96cef3f7f0195d417a6a47e2c
dnfra25 <francesco.dinapoli@mail.polimi.it>
2026-07-12 01:28:32 +0200

    Update Boundary.cpp

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -156,6 +156,7 @@ void Boundary::bounceBackRight(Lattice& lattice)
 
 void Boundary::movingTop(Lattice& lattice)
 {
+
     auto& f = lattice.distributions();
 
 
@@ -163,83 +164,79 @@ void Boundary::movingTop(Lattice& lattice)
 
 
     #pragma omp parallel for
-    for(int x = 1; x < nx-1; ++x)
+    for(int x=1; x<nx-1; ++x)
     {
 
         /*
-            Zou-He velocity boundary condition
+            Top wall:
 
-            top wall:
-                uy = 0
-                ux = lidVelocity
+            known:
+                f0
+                f1
+                f2
+                f4
+                f6
+                f8
 
+            unknown:
+                f3
+                f5
+                f7
+
+            ux = lidVelocity
+            uy = 0
         */
 
 
         double rho =
-            f[lattice.index(0,x,y)]
-          +
-            f[lattice.index(1,x,y)]
-          +
+              f[lattice.index(0,x,y)]
+            + f[lattice.index(1,x,y)]
+            + f[lattice.index(2,x,y)]
+            + 2.0 *
+              (
+                 f[lattice.index(4,x,y)]
+               + f[lattice.index(6,x,y)]
+               + f[lattice.index(8,x,y)]
+              );
+
+
+        // north population
+        f[lattice.index(3,x,y)]
+        =
+        f[lattice.index(4,x,y)];
+
+
+        // north-east
+        f[lattice.index(5,x,y)]
+        =
+        f[lattice.index(6,x,y)]
+        +
+        0.5*
+        (
             f[lattice.index(2,x,y)]
-          +
-            2.0 *
-            (
-                f[lattice.index(4,x,y)]
-              +
-                f[lattice.index(7,x,y)]
-              +
-                f[lattice.index(8,x,y)]
-            );
-
-
-        // south population
-        f[lattice.index(3,x,y)] =
-            f[lattice.index(4,x,y)]
-            +
-            (2.0/3.0)
-            *
-            rho
-            *
-            0.0;
+          -
+            f[lattice.index(1,x,y)]
+        )
+        +
+        (rho*lidVelocity)/6.0;
 
 
 
         // north-west
-        f[lattice.index(6,x,y)] =
-            f[lattice.index(8,x,y)]
-            -
-            0.5 *
-            (
-                f[lattice.index(1,x,y)]
-              -
-                f[lattice.index(2,x,y)]
-            )
-            -
-            (1.0/6.0)
-            *
-            rho
-            *
-            lidVelocity;
+        f[lattice.index(7,x,y)]
+        =
+        f[lattice.index(8,x,y)]
+        +
+        0.5*
+        (
+            f[lattice.index(1,x,y)]
+          -
+            f[lattice.index(2,x,y)]
+        )
+        -
+        (rho*lidVelocity)/6.0;
 
-
-
-        // north-east
-        f[lattice.index(5,x,y)] =
-            f[lattice.index(7,x,y)]
-            +
-            0.5 *
-            (
-                f[lattice.index(1,x,y)]
-              -
-                f[lattice.index(2,x,y)]
-            )
-            +
-            (1.0/6.0)
-            *
-            rho
-            *
-            lidVelocity;
 
     }
+
 }
