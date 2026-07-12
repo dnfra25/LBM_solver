09ec066bd38b0378fd7321844819a8427fa2a03a
dnfra25 <francesco.dinapoli@mail.polimi.it>
2026-07-12 12:50:53 +0200

    Update Parameters.hpp

--- include/Parameters.hpp
+++ include/Parameters.hpp
@@ -5,98 +5,69 @@
 namespace Parameters
 {
 
-    // -----------------------------
-    // Lattice size
-    // -----------------------------
-
-    constexpr int Nx = 200;
-    constexpr int Ny = 200;
+constexpr int Nx = 200;
+constexpr int Ny = 200;
 
 
-    // -----------------------------
-    // Physical parameters
-    // -----------------------------
-
-    // Lid velocity
-    constexpr double U_lid = 0.1;
+constexpr double U_lid = 0.1;
 
 
-    // Reference length
-    // for cavity: L = Nx-1 lattice units
-    constexpr double L = Nx - 1;
+constexpr double L = Nx - 1;
 
 
-    // Reynolds number
-    constexpr double Re = 100.0;
+constexpr double Re = 100.0;
 
 
 
-    // -----------------------------
-    // LBM parameters
-    // -----------------------------
+// LBM
 
-    // Speed of sound squared
-    constexpr double cs2 = 1.0/3.0;
+constexpr double cs2 = 1.0/3.0;
 
 
-    // Kinematic viscosity
-    constexpr double nu =
-        U_lid * L / Re;
+constexpr double nu =
+    U_lid * L / Re;
 
 
 
-    // Relaxation time
-    // nu = cs2*(tau-0.5)
-
-    constexpr double tau =
-        0.5 + nu/cs2;
+constexpr double tau =
+    0.5 + nu/cs2;
 
 
 
-    // TRT parameters
+// TRT
 
-    constexpr double lambda_trt = 0.25;
-
-
-    constexpr double tau_p = tau;
+constexpr double lambda_trt = 0.25;
 
 
-    constexpr double tau_m =
-        lambda_trt/(tau_p-0.5)+0.5;
+constexpr double tau_p =
+    tau;
 
 
-    constexpr double omega_p =
-        1.0/tau_p;
+constexpr double tau_m =
+    lambda_trt/(tau_p-0.5)+0.5;
 
 
-    constexpr double omega_m =
-        1.0/tau_m;
+constexpr double omega_p =
+    1.0/tau_p;
 
 
-
-    // -----------------------------
-    // Simulation control
-    // -----------------------------
-
-    constexpr int max_iterations = 50000;
-
-
-    // check convergence every N iterations
-    constexpr int output_interval = 500;
-
-
-    // convergence tolerance
-    constexpr double tolerance = 1.0e-8;
+constexpr double omega_m =
+    1.0/tau_m;
 
 
 
-    // Initial density
+constexpr int max_iterations = 50000;
 
-    constexpr double rho0 = 1.0;
+
+constexpr int output_interval = 500;
+
+
+constexpr double tolerance = 1.0e-8;
+
+
+constexpr double rho0 = 1.0;
 
 
 }
-
-
 
 #endif
